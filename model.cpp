69948b237efcec762d68fa8a03ddd816ab84d123
Ryan Huang <pi88031221@gmail.com>
2025-11-06 20:27:46 +0800

    add naive bayes

--- model.cpp
+++ model.cpp
@@ -3,19 +3,30 @@
 #include "model.h"
 using namespace std;
 
+#define PI acos(-1)
+
 NB NB_train(vector<double> &data) {
-    // TODO find mean and sigma of data
+    int n = data.size();
+
     NB nb;
-    double mean = 0;
-    double sigma = 0;
+    double mean=0, sigma;
+    for (int i=0; i<n; i++) {
+        mean += data[i];
+    }
+    mean /= data.size();
+
+    for (int i=0; i<n; i++) {
+        sigma += (data[i] - mean) * (data[i] - mean);
+    }
+    sigma = sqrt(sigma/n);
 
     nb.mean = mean;
     nb.sigma = sigma;
     return nb;
 }
 
-double NB_eval(vector<vector<double>> &test_data) {
-
+double NB_eval(double x, NB nb) {
+    return exp(-(x-nb.mean)*(x-nb.mean)/(2*nb.sigma*nb.sigma)) / (nb.sigma * sqrt(2*PI));
 }
 
 // (DT-BF based adaboost)
